001048e80bbb307321854b13846dba106594ab67
Nihal Jadhav <nihal.jadhav96@gmail.com>
2025-03-12 14:41:59 +0530

    add theme management with toggle functionality; implement light and dark themes using Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useAppSelector } from "./redux/hooks";
+import { RootState } from './redux/store'
+import { lightTheme, darkTheme } from "./styles/theme";
 import Dashboard from '@pages/Dashboard'
 import Router from './routes'
 
 function App() {
+  const themeMode = useAppSelector((state: RootState) => state.theme.mode);
   return (
-    <>
+    <ThemeProvider theme={themeMode === "light" ? lightTheme : darkTheme}>
+      <CssBaseline />
       <Dashboard />
       <Router />
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,20 +1,44 @@
 import React from 'react'
+import { Stack, Button, FormGroup, FormControlLabel, useTheme } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import { useAppSelector, useAppDispatch } from '../redux/hooks'
+import { toggleTheme } from '../redux/slices/themeSlice'
+import { RootState } from '../redux/store'
+import MaterialUISwitch from '../components/MuiSwitch'
 
 const Dashboard: React.FC = () => {
   const navigate = useNavigate()
-
+  const dispatch = useAppDispatch();
+  const themeMode = useAppSelector((state: RootState) => state.theme.mode);
+  const theme = useTheme();
+  console.log('Theme', theme.palette.primary.main)
   return (
-    <nav style={{ display: 'flex', justifyContent: 'center' }}>
-      <ul style={{ display: 'flex', listStyleType: 'none', padding: 0 }}>
-        <li style={{ marginRight: '20px', cursor: 'pointer' }} onClick={() => navigate('/')}>
-          Home
-        </li>
-        <li style={{ cursor: 'pointer' }} onClick={() => navigate('/about')}>
-          About
-        </li>
-      </ul>
-    </nav>
+    <Stack
+      direction='row'
+      justifyContent='center'
+      alignItems='center'
+      spacing={2}
+      sx={{ padding: 2 }}
+    >
+      <Button
+        variant='text'
+        onClick={() => navigate('/')}
+        sx={{ color: theme.palette.primary.main }}
+      >
+        Home
+      </Button>
+      <Button
+        variant='text'
+        onClick={() => navigate('/about')}
+        sx={{ color: theme.palette.primary.main }}
+      >
+        About
+      </Button>
+      <FormGroup>
+        <FormControlLabel control={<MaterialUISwitch checked={themeMode === "dark"}
+          onChange={() => dispatch(toggleTheme())} />} label={`Change To ${themeMode === "dark" ? 'Light' : 'Dark'}`} />
+      </FormGroup>
+    </Stack>
   )
 }
 
